e41294cf3c0df9ca024a9b7862d1b66ef9433af3
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-11-03 11:17:03 -0600

    [app.js] add ability for rocket to move

--- app.js
+++ app.js
@@ -29,49 +29,105 @@ class Rocket {
     }
 }
 
-const rocket = new Rocket(300, 525, 'white', 40, 90);
+const rocket = new Rocket(300, 425, 'white', 40, 90);
+
 rocket.render();
 
-class Obsticals {
-    constructor(x, y, color, width, height) {  // Change color to image later
-        this.x = x
-        this.y = y
-        this.color = color  // Change to sprite image later
-        this.width = width
-        this.height = height
+document.addEventListener('keydown', function(evt) {
+    if (evt.key === 'w') {
+        rocket.y -= 10
+    } else if (evt.key === 'a') {
+        rocket.x -= 10
+    } else if (evt.key === 's') {
+        rocket.y += 10
+    } else if (evt.key === 'd') {
+        rocket.x +=10
     }
-    render() {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.width, this.height)
+})
+
+document.addEventListener('keyup', function(evt) {
+    if (evt.key === 'w') {
+        rocket.y -= 10
+    } else if (evt.key === 'a') {
+        rocket.x -= 10
+    } else if (evt.key === 's') {
+        rocket.y += 10
+    } else if (evt.key === 'd') {
+        rocket.x +=10
     }
+})
+
+function rePaint() {
+    ctx.clearRect(0, 0, game.width, game.height)
+    rocket.render()
 }
 
-const satellite = new Obsticals()
-const ufo = new Obsticals()
-const meteor = new Obsticals()
+setInterval(rePaint, 1000 / 60) 
 
 
 
-// Scrolling image
-var img = new Image(); 
-img.src = "space6.png";
+
+
+
+
+
+
+
+
+
+
+
+
+// class Obsticals {
+//     constructor(x, y, color, width, height) {  // Change color to image later
+//         this.x = x
+//         this.y = y
+//         this.color = color  // Change to sprite image later
+//         this.width = width
+//         this.height = height
+//     }
+//     render() {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(this.x, this.y, this.width, this.height)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Scrolling image
+// var img = new Image(); 
+// img.src = "space6.png";
   
-window.onload = function() { 
-    var imgHeight = 0; 
-    var scrollSpeed = 1.5; 
+// window.onload = function() { 
+//     var imgHeight = 0; 
+//     var scrollSpeed = 1.5; 
   
-    function loop() { 
-        ctx.drawImage(img, 0, imgHeight); 
-        ctx.drawImage(img, 0, imgHeight - game.height); 
-        imgHeight += scrollSpeed; 
+//     function loop() { 
+//         ctx.drawImage(img, 0, imgHeight); 
+//         ctx.drawImage(img, 0, imgHeight - game.height); 
+//         imgHeight += scrollSpeed; 
   
-       // resets the image 
-        if (imgHeight == game.height) 
-            imgHeight = 0; 
-        window.requestAnimationFrame(loop); 
-    } 
-    loop();
-} 
+//        // resets the image 
+//         if (imgHeight == game.height) 
+//             imgHeight = 0; 
+//         window.requestAnimationFrame(loop); 
+//     } 
+//     loop();
+// } 
 
 
 
