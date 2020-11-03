c4a24a8ab696e09a3a9fff61464282da1985ba05
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-11-03 16:59:19 -0600

    [app.js] Add moving obstical to screenand recoded canvas animation

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ game.height = height.replace('px', '');
 
 game.width = width.replace('px', '');
 
-// Grabbing context from the canvas
 const ctx = game.getContext('2d');
 
 class Rocket {
@@ -29,9 +28,7 @@ class Rocket {
     }
 }
 
-const rocket = new Rocket(300, 425, 'white', 40, 90);
-
-rocket.render();
+const rocket = new Rocket(300, 425, 'white', 30, 90);
 
 document.addEventListener('keydown', function(evt) {
     if (evt.key === 'w') {
@@ -57,81 +54,79 @@ document.addEventListener('keyup', function(evt) {
     }
 })
 
-function rePaint() {
-    ctx.clearRect(0, 0, game.width, game.height)
-    rocket.render()
-}
+var posX = 20, posY = 100;
 
-setInterval(rePaint, 1000 / 60) 
-
-
-
-
-
-
-
-
-
-
-
+setInterval(function() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,game.width, game.height);
+    posX += 6;
+    posY += 4;
+    ctx.beginPath();
+    ctx.fillStyle = "orange";
+    ctx.arc(posX, posY, 20, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.fill();
+    rocket.render();
+    
+}, 30);
 
 
 
-
-// class Obsticals {
-//     constructor(x, y, color, width, height) {  // Change color to image later
-//         this.x = x
-//         this.y = y
-//         this.color = color  // Change to sprite image later
-//         this.width = width
-//         this.height = height
-//     }
-//     render() {
-//         ctx.fillStyle = this.color
-//         ctx.fillRect(this.x, this.y, this.width, this.height)
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// EXTRA CODE I DIDNT WANT TO DELETE BUT MIGHT USE LATER 
 
 // // Scrolling image
 // var img = new Image(); 
 // img.src = "space6.png";
-  
+
 // window.onload = function() { 
-//     var imgHeight = 0; 
-//     var scrollSpeed = 1.5; 
-  
-//     function loop() { 
-//         ctx.drawImage(img, 0, imgHeight); 
-//         ctx.drawImage(img, 0, imgHeight - game.height); 
-//         imgHeight += scrollSpeed; 
-  
-//        // resets the image 
-//         if (imgHeight == game.height) 
-//             imgHeight = 0; 
-//         window.requestAnimationFrame(loop); 
-//     } 
-//     loop();
-// } 
+    //     var imgHeight = 0; 
+    //     var scrollSpeed = 1.5; 
+    
+    //     function loop() { 
+        //         ctx.drawImage(img, 0, imgHeight); 
+        //         ctx.drawImage(img, 0, imgHeight - game.height); 
+        //         imgHeight += scrollSpeed; 
+        
+        //        // resets the image 
+        //         if (imgHeight == game.height) 
+        //             imgHeight = 0; 
+        //         window.requestAnimationFrame(loop); 
+        //     } 
+        //     loop();
+        // } 
+        
+        
+        // function rePaint() {
+            //     ctx.clearRect(0, 0, game.width, game.height)
+            
+            
+            
+            //     // rocket.render();
+            //     // meteor.render();
+            
+            // }
+            
+            // setInterval(rePaint, 1000 / 60) 
+            
+            
+            
+            // class Obsticals {
+                //     constructor(x, y, color, width, height, velocity) {  // Change color to image later
+                //         this.x = x
+                //         this.y = y
+                //         this.color = color  // Change to sprite image later
+                //         this.width = width
+                //         this.height = height
+                //         this.velocity = velocity
+                //     }
+                //     render() {
+                    //         ctx.fillStyle = this.color
+                    //         ctx.fillRect(this.x, this.y, this.width, this.height)
+                    //     }
+                    // }
+                    
+            
+                    // const meteor = new Obsticals(200, 325, 'grey', 20, 20);
+                    
 
-
-
-
-
-
-
+                    // meteor.render();
